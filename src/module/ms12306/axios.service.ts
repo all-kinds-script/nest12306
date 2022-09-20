1953664d64e4eb8f9107416311de692612c13575
MiniOcean404 <1037306928@qq.com>
2022-09-21 00:34:44 +0800

    feat: 修改 axios 加载配置方式

--- src/module/ms12306/axios.service.ts
+++ src/module/ms12306/axios.service.ts
@@ -3,8 +3,8 @@ import { HttpService } from '@nestjs/axios'
 import { map, Observable } from 'rxjs'
 import { AxiosResponse } from 'axios'
 import { Buffer } from 'buffer'
-import chalk from 'chalk'
 
+// this.axios.axiosRef 可获取 axios 实例，例： this.axios.axiosRef.interceptors
 @Injectable()
 export class AxiosService {
     constructor(private readonly axios: HttpService) {}
