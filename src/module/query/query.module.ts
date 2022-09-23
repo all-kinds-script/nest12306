928337e571d4faae5c42c3632e7e2436607892a1
戴海洋 <haiyang.dai@davincimotor.com>
2022-09-23 18:53:58 +0800

    feat: 定时任务，日志

--- src/module/query/query.module.ts
+++ src/module/query/query.module.ts
@@ -3,9 +3,10 @@ import { QueryService } from './query.service'
 import { AxiosModule } from '@/module/axios/axios.module'
 import { MsConfigModule } from '@/module/config/ms-config.module'
 import { EmailService } from '@/module/email/email.service'
+import ScheduleQueryService from '@/module/query/schedule-query.service'
 
 @Module({
     imports: [AxiosModule, MsConfigModule],
-    providers: [QueryService, EmailService],
+    providers: [QueryService, EmailService, ScheduleQueryService],
 })
 export class QueryModule {}
---
+++ src/module/query/schedule-query.service.ts
@@ -0,0 +1,23 @@
+import { Inject, Injectable } from '@nestjs/common'
+import { SchedulerRegistry } from '@nestjs/schedule'
+import { CronCommand, CronJob } from 'cron'
+import { Logger } from 'winston'
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston'
+
+@Injectable()
+export default class ScheduleQueryService {
+    constructor(
+        private scheduler: SchedulerRegistry,
+        @Inject(WINSTON_MODULE_PROVIDER)
+        private readonly logger: Logger
+    ) {
+        this.logger.info(`我是日志`)
+    }
+
+    public queryTicket(name: string, seconds: number, tick: CronCommand) {
+        const job = new CronJob(`*/${seconds} * * * * *`, tick)
+
+        this.scheduler.addCronJob(name, job)
+        job.start()
+    }
+}
