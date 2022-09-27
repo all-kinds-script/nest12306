bdff82fba21c3e5e4eef2d6600d3bdb8fb0834f7
戴海洋 <haiyang.dai@davincimotor.com>
2022-09-27 14:18:15 +0800

    feat: 统一设置头

---
+++ src/module/axios/axios.service.ts
@@ -0,0 +1,32 @@
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common'
+import { HttpService } from '@nestjs/axios'
+import { AxiosInstance } from 'axios'
+import { Cache } from 'cache-manager'
+import { GenericsObject } from '@/typings/common'
+
+@Injectable()
+export class AxiosService {
+    private readonly axiosRef: AxiosInstance
+    private cookies: GenericsObject<string> = {}
+
+    constructor(private readonly axios: HttpService, @Inject(CACHE_MANAGER) private readonly cacheManage: Cache) {
+        this.axiosRef = this.axios.axiosRef
+    }
+
+    set cookieHeader({ key, value }) {
+        this.cookies[key] = value
+        let cookieStr = ''
+
+        for (const [key, value] of Object.entries(this.cookies)) {
+            cookieStr += `${key}=${value};`
+        }
+
+        this.axiosRef.defaults.headers.common['cookie'] = cookieStr
+
+        this.cacheManage.set(key, this.cookies).then()
+    }
+
+    get cookie() {
+        return this.cookies
+    }
+}
